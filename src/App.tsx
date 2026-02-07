39796ecdd5103f1e0d7dfe09e5ae6adf11a8a37d
Muhammed BABAN <mbbn4557@gmail.com>
2026-02-08 00:50:18 +0300

    feat: adding some logic to test local ai

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData]: any = useState(null)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Sistem çalışıyor: " + count);
+    }, 1000);
+    
+  }, []);
+
+  const expensiveCalculation = () => {
+    let result = 0;
+    for (let i = 0; i < 1000000; i++) {
+      result += i;
+    }
+    return result;
+  };
 
   return (
     <>
@@ -12,22 +28,14 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Hesaplanan Değer: {expensiveCalculation()}</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
